d0f8d51a83325d150c6607aa9b4d41016c061870
takagotch <dyaccb@gmail.com>
2018-08-30 12:40:54 +0900

    $.plot(, data, options);

---
+++ tut/fr.js
@@ -0,0 +1,11 @@
+// <!--[if lte IE 8]<script language="javascript" type="text/javascript" src="excanvas.min.js">!>
+
+// <div id="placeholder"></div>
+// <div id="placeholder" style="width:600px;height:300px"></div>
+$.plot($("#placeholder"), data, options);
+
+$.plot($("#placeholder"), [ [0, 0], [1, 1] ], { yaxis: { max: 1 }});
+
+
+
+
